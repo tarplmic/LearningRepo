6bc5c9d40cf9a6ae40a802c1e482241bee26a84e
Michaela Tarpley <michaela.tarpley@gmail.com>
2019-01-03 19:43:00 -0600

    My first program which is a console app that pretends to be hacked.

---
+++ 001_HackedConsole/ConsoleApp1/MichaelaFirstApp.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackedConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(120, 37);
+            Console.ReadLine();
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+            //Console.WriteLine("DumbDumbFace");
+            //Console.ReadLine();
+            Console.WriteLine("what you are about to see has not been seen by anyone ever");
+            Console.ReadLine();
+            Console.WriteLine("just kidding. ur an id10t");
+            Console.ReadLine();
+            Console.WriteLine();
+          
+            Console.Write("Hello World");
+            Console.ReadLine();
+            Console.Write("what you are about to see has not been seen by anyone ever");
+            Console.ReadLine();
+            Console.Write("just kidding. ur an id10t");
+            Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("H");
+            Console.ReadLine();
+
+            Console.Write("E");
+            Console.ReadLine();
+
+            Console.Write("L");
+            Console.ReadLine();
+
+            Console.Write("L");
+            Console.ReadLine();
+
+            Console.Write("O");
+            Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            Console.WriteLine("Okay, we're back on");
+            Console.ReadLine();
+            Console.Beep();
+            Console.Beep();
+            Console.WriteLine("Did you hear that?");
+            Console.ReadLine();
+            Console.Beep(); Console.Beep(); Console.Beep();
+            Console.WriteLine("What in the world...");
+            Console.ReadLine();
+            Console.Beep(); Console.Beep(); Console.Beep(); Console.Beep(); Console.Beep();
+            Console.Write("Well, it looks like we've been hackedddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
+            Console.ReadLine();
+
+            Console.Beep();
+            Console.Clear();
+            Console.Beep();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            Console.SetWindowSize(40,16);
+            Console.WriteLine();
+            Console.ReadLine();
+
+            Console.SetWindowSize(120,37);
+            Console.WriteLine("Woah...");
+            Console.ReadLine();
+            Console.WriteLine("What the heck!!!");
+            Console.ReadLine();
+            Console.WriteLine("Honey, I shrunk the window");
+            Console.ReadLine();
+        }
+    }
+}
