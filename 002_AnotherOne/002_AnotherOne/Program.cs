3e8ce979b3840b239843b500e51571c6890c279e
Michaela Tarpley <michaela.tarpley@gmail.com>
2019-01-09 20:54:39 -0600

    added "this is foreground color" "this is background color"

--- 002_AnotherOne/002_AnotherOne/Program.cs
+++ 002_AnotherOne/002_AnotherOne/Program.cs
@@ -10,6 +10,26 @@ namespace _002_AnotherOne
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+            Console.Beep();
+            Console.SetWindowSize(105, 30);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("hi");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("hi)");
+            Console.ReadLine();
+            Console.ResetColor();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("this is background");
+            Console.ReadLine();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("this is foreground color");
+            Console.ReadLine();
+            
+       
         }
     }
 }
