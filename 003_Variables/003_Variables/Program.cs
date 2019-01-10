4d5ab42f5a2b372d935976fa93de689c63429bb3
Michaela Tarpley <michaela.tarpley@gmail.com>
2019-01-09 22:24:10 -0600

    From the variable lesson. x, y, first and last name

---
+++ 003_Variables/003_Variables/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+            x = 7;
+            y = x + 3;
+            Console.WriteLine(y);
+            Console.ReadLine();
+            Console.WriteLine(x + 2);
+            Console.ReadLine();
+            Console.WriteLine(x + 3);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name:");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Type your last name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + firstName + " " + lastName);
+            Console.ReadLine();
+
+        }
+    }
+}
