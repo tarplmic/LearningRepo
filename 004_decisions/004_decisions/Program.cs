7d2865cf29ba18816a8fe05d4cb51693092f597f
Michaela Tarpley <michaela.tarpley@gmail.com>
2019-01-11 14:33:16 -0600

    Commented out last version of program. Made a working game of "whats your favorite color?".

--- 004_decisions/004_decisions/Program.cs
+++ 004_decisions/004_decisions/Program.cs
@@ -8,10 +8,10 @@ namespace _004_decisions
         {
            
             {
-                Console.WriteLine("Michaela's Big Giveaway");
+                /*Console.WriteLine("Michaela's Big Giveaway");
                 Console.Write("Choose a door: 1, 2, or 3:");
                 string doorAnswer = Console.ReadLine();
-                /*string message;
+                string message;
 
                 if (doorAnswer == "1")
                 {
@@ -31,11 +31,79 @@ namespace _004_decisions
                 }
 
                 Console.Write(message);
-                Console.ReadLine(); */
+                Console.ReadLine();
 
                 string message = (doorAnswer == "1") ? "boat" : "poop";
                 Console.WriteLine("You entered: {0}, therefore you won a {1}.", doorAnswer, message);
-                Console.ReadLine();
+                Console.ReadLine();*/
+
+
+                Console.WriteLine("Type 'quit' when you are finished with the game");
+                bool keepGoing = true;
+
+                while (keepGoing == true)
+                {
+                    
+                    Console.Write("What is your favorite color:");
+                    bool showFinalText = true;
+                    string Answer = Console.ReadLine();
+
+
+                    if (Answer == "blue")
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                    }
+                    else if (Answer == "red")
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                    }
+                    else if (Answer == "yellow")
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                    }
+                    else if (Answer == "white")
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                    }
+                    else if (Answer == "green")
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                    }
+                    else if (Answer == "purple")
+                    {
+                        Console.WriteLine("I'm sorry. I don't know that color. Restart program and try using 'magenta'");
+                        Console.ReadLine();
+                        showFinalText = false;
+                    }
+                    else if (Answer == "magenta")
+                    {
+                        Console.BackgroundColor = ConsoleColor.Magenta;
+                    }
+                    else if (Answer == "quit")
+                    {
+                        keepGoing = false;
+                        showFinalText = false; 
+                        Console.WriteLine("Ok, see ya later");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("I'm sorry. I don't know that color.");
+                        Console.ReadLine();
+                        showFinalText = false;
+                    }
+
+                    if (showFinalText == true)
+                    {
+                        Console.WriteLine("Here ya go.");
+                        Console.ReadLine();
+
+                    }
+                    Console.ResetColor();
+
+
+                }
+                 
             }
         }
     }
