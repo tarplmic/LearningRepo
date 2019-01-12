abc5b8a970ffa63b39a5d3511d3b914f1ebb2489
Michaela Tarpley <michaela.tarpley@gmail.com>
2019-01-12 17:13:11 -0600

    Made from the "for iterations" lesson in the video. Short program using for (int i = 0; i < 10; i++)

---
+++ 005_MessingAround/005_MessingAround/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_MessingAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ReadLine();
+            }
+
+            
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+
+
+            }
+            Console.ReadLine();
+           
+
+          
+        }
+
+    }
+}
