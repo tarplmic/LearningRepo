b349a71c4410744e101324f6dc54e0ebc34a89de
Michaela Tarpley <michaela.tarpley@gmail.com>
2019-01-12 19:31:29 -0600

    Most complex program that was my idea. Simple math game adding x+y=#. # increases by 1 every time until it reaches 5 calculations, then, x increases by 1 and y increases by 1 to do another 5 calculations and so on and so on.

---
+++ 006_SimpleMathGame/006_SimpleMathGame/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace _006_SimpleMathGame
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            for (int z = 5; z < 1000;)
+            {
+                for (int y = 1; y < 100;)
+                {
+                    for (int x = 0; x < z;)
+                    {
+                        Console.WriteLine("What is " + x + "+" + y + ":");
+
+                        int Answer = Convert.ToInt32(Console.ReadLine());
+
+                        if (Answer == x + y)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Correct!");
+                            x = x + y;
+                            
+                        }
+                        
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Incorrect");
+                        }
+
+                        Console.ResetColor();
+                        Console.ReadLine();
+
+                       
+                    }
+                    y = y + 1;
+                    z = z + 5;
+                }
+               
+            }
+
+            /*Console.WriteLine("What is " + x + "+1: ");
+
+            int Answer = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 0; x < 5;)
+            {
+                if (Answer == x + 1)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Correct!");
+                    x = x + 1;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Incorrect");
+                }
+            }
+
+            for (int x = 0; x < 10;)
+            {
+                Console.WriteLine("What is " + x + "+2: ");
+                int Answer = Convert.ToInt32(Console.ReadLine());
+
+                if (Answer == x + 2)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Correct!");
+                    x = x + 2;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Incorrect");
+                }
+
+                Console.ResetColor();
+                Console.ReadLine();
+            } */
+
+            /*
+                bool functionOnePlusOne = true;
+                while (functionOnePlusOne = true)
+                {
+                    Console.WriteLine("What is 1+1: ");
+                    string Answer = Console.ReadLine();
+
+                    if (Answer == "2")
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Correct!");
+                        Console.ResetColor();
+                        break;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Incorrect");
+                    }
+                }
+                Console.ReadLine();
+                Console.ResetColor();
+
+            bool functionTwoPlusOne = true;
+            while (functionTwoPlusOne = true)
+            {
+                Console.WriteLine("What is 2+1: ");
+                string Answer = Console.ReadLine();
+
+                if (Answer == "3")
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Correct!");
+                    Console.ResetColor();
+                    break;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Incorrect");
+                }
+            }
+            Console.ReadLine();
+            Console.ResetColor();
+
+            Console.WriteLine("next code");
+            Console.ReadLine();
+            */
+        }
+    }
+}
