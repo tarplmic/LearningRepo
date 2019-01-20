5d814739036757c0d8c5a289bf794e3e3d041afa
Michaela Tarpley <michaela.tarpley@gmail.com>
2019-01-19 22:59:29 -0600

    Creating methods for the first time. One method takes in string and converts to a reversed character array, other method writes the string of the result from the first method.

---
+++ 009_SimpleMethod/009_SimpleMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009_SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Philip = "Philip";
+            ReverseStringandPrint("Michaela");
+            ReverseStringandPrint("Victoria");
+            ReverseStringandPrint("Brianna");
+            ReverseStringandPrint(Philip);
+
+        }
+
+        private static void ReverseStringandPrint(string message)
+        {
+            Console.WriteLine(ReverseString(message));
+            Console.ReadLine();
+        }
+        
+        private static string ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+
+            return String.Concat(messageArray);
+        }
+        
+    }
+}
