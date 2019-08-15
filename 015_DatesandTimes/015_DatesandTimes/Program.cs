18b98581702d05299fa2948b87501a33f69419aa
Michaela Tarpley <michaela.tarpley@gmail.com>
2019-08-14 19:46:29 -0500

    Learning dates and times

---
+++ 015_DatesandTimes/015_DatesandTimes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015_DatesandTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToLongTimeString());
+
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(-12).ToString());
+            //Console.WriteLine(myValue.Month);
+
+            DateTime myBirthday = new DateTime(1999, 11, 6);
+            //Console.WriteLine(myBirthday.ToShortDateString());
+            //DateTime myBirthday = DateTime.Parse("8/31/2001");
+
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalDays);
+            Console.ReadLine(); 
+        }
+    }
+}
