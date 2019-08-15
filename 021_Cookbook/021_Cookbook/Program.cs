e5b1b2189cdb2eadc3b717b0d521e9236e1d250a
Michaela Tarpley <michaela.tarpley@gmail.com>
2019-08-14 19:49:49 -0500

    Very beginning stages for using classes to implement a cookbook app.

---
+++ 021_Cookbook/021_Cookbook/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _021_Cookbook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class NameOfFood
+    {
+        public string name { get; set; }
+
+    }
+    class Ingredients
+    {
+        public string ingredientList { get; set; }
+
+    }
+    class Instructions
+    {
+        public string firstStep { get; set; }
+        public string secondStep { get; set; }
+        public string thirdStep { get; set; }
+
+    }
+}
